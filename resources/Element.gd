9534afd658de2aba52f78a6d30138f64cbbc7799
Wake1st <joeltwakefield@gmail.com>
2025-07-26 20:08:57 -0600

    elements and balancing

---
+++ resources/Element.gd
@@ -0,0 +1,9 @@
+class_name Element
+
+
+enum Types {
+	FIRE,
+	AIR,
+	WATER,
+	EARTH
+}
--- resources/Tarot.gd
+++ resources/Tarot.gd
@@ -3,5 +3,8 @@ extends Resource
 
 
 @export var name: String
+@export var element: Element.Types
 @export_multiline var upright: String
 @export_multiline var reversed: String
+
+var balanced: bool = true
